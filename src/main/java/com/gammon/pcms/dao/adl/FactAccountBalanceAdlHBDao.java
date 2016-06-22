c92a8f4bfd7ca47c3815224e31493a740694c207
Paul yiu <paul.yiu@gammonconstruction.com>
2016-06-22 06:23:15 +0000

    padding white space for getFactAccountBalance...'s parameter

--- src/main/java/com/gammon/pcms/dao/adl/FactAccountBalanceAdlHBDao.java
+++ src/main/java/com/gammon/pcms/dao/adl/FactAccountBalanceAdlHBDao.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang.StringUtils;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import com.gammon.pcms.model.adl.FactAccountBalance;
 import com.gammon.qs.application.exception.DatabaseOperationException;
+
+import freemarker.template.utility.StringUtil;
 @Repository
 public class FactAccountBalanceAdlHBDao extends BaseAdlHibernateDao<FactAccountBalance> {
 	
@@ -23,6 +26,8 @@ public class FactAccountBalanceAdlHBDao extends BaseAdlHibernateDao<FactAccountB
 	public List<FactAccountBalance> findByFiscalYearAndAccountTypeLedgerAndEntityBusinessUnitKeyAndAccountSubsidiaryNot(
 			BigDecimal fiscalYear, String accountTypeLedger, String entityBusinessUnitKey, String accountSubsidiary) throws DatabaseOperationException{
 		List<FactAccountBalance> factAccountBalanceList = new ArrayList<FactAccountBalance>();
+		entityBusinessUnitKey = StringUtils.leftPad(StringUtils.defaultString(entityBusinessUnitKey), 12);
+		accountSubsidiary = StringUtil.leftPad(StringUtils.defaultString(accountSubsidiary), 8);
 		Criteria criteria = getSession().createCriteria(getType());
 		criteria.add(Restrictions.eq("fiscalYear", fiscalYear));
 		criteria.add(Restrictions.eq("accountTypeLedger", accountTypeLedger));
