08f7ee1e4e0d40b42afcbe907ddd01623b08d834
Tiky Wong <tiky.wong@gammonconstruction.com>
2016-09-13 07:13:21 +0000

    SQL: Revised for Roll-Out

--- SQLs/rollout/05-Gammon Security Framework/05-01-insert_into_ac_application.sql
+++
@@ -1,6 +0,0 @@
---------------------------------------------------------
---  File created - Monday-August-22-2016   
---------------------------------------------------------
-REM INSERTING into ACDATAUAT.AC_APPLICATION
-SET DEFINE OFF;
-Insert into ACDATAUAT.AC_APPLICATION (APPLICATIONCODE,APPLICATIONNAME,STATUS,AUTHENTICATIONMETHOD,SECURITYTYPE,MAINTENANCEWINDOW,MESSAGE,HYPERLINK,STAFFIDREQUIRED,ADDITIONALINFORMATION,LASTMODIFYDATE,LASTMODIFYUSER) values ('PCMS','Project Cost Management System','A','WIN','MIXED',null,null,'http://pcms','Y',null,to_timestamp('19-AUG-16 02.00.28.344052000 PM','DD-MON-RR HH.MI.SSXFF AM'),'GAMSKA\tikywong');
