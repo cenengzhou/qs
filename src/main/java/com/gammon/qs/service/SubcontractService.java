a0fe6af6dec7edf27e3f6157e4334f23c25db3dd
Paul yiu <paul.yiu@gammonconstruction.com>
2016-11-18 09:16:56 +0000

    fix cannot save subcontract header

--- src/main/java/com/gammon/qs/service/SubcontractService.java
+++ src/main/java/com/gammon/qs/service/SubcontractService.java
@@ -3655,8 +3655,10 @@ public class SubcontractService {
 			//check if subcontract is submitted or awarded
 			Subcontract subcontractInDB = subcontractHBDao.obtainSubcontract(jobNo, subcontract.getPackageNo());
 			if(subcontractInDB != null){
-				if(subcontractInDB.getSubcontractStatus() != null && subcontractInDB.getSubcontractStatus() == 330 || subcontractInDB.getSubcontractStatus() == 500){
-					return "Subcontract has been awarded or submitted.";
+				if(subcontractInDB.getSubcontractStatus() != null){
+					if(subcontractInDB.getSubcontractStatus() == 330 || subcontractInDB.getSubcontractStatus() == 500){
+						return "Subcontract has been awarded or submitted.";
+					}
 				}
 				
 				subcontractInDB.setDescription(subcontract.getDescription());
