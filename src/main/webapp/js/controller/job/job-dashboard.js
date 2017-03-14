9ab22b796e6ef3a40f6aacc75f4a9e35b4319758
Koey Yeung <koey.yeung@gammonconstruction.com>
2017-03-14 02:03:46 +0000

    Job Dashboard: Change wordings

--- src/main/webapp/js/controller/job/job-dashboard.js
+++ src/main/webapp/js/controller/job/job-dashboard.js
@@ -81,7 +81,7 @@ mainApp.controller('JobDashboardCtrl', ['$scope', 'colorCode', 'jobService', 'ad
     	
     	$scope.chartParameters = {
     			labels : ['Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    			series : ['Total Budget', 'Internal Value', 'Gross Certified Value', 'Cost'],//  'Tender Budget'
+    			series : ['Total Budget', 'Internal Value', 'Contract Receivable', 'Actual Value'],//  'Tender Budget'
     			//data : data.data,
     			data: [totalBudgetList, turnoverList, contractReceivableList, actualValueList],
     			options : {
