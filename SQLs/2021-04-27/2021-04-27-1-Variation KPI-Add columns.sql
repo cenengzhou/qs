56870a99fce415ea8e0a364ab2039e182f3cc6fb
Michael Lau <michaellaugammon@gmail.com>
2021-04-27 12:24:53 +0800

    Prepare SQL script for release this week

---
+++ SQLs/2021-04-27/2021-04-27-1-Variation KPI-Add columns.sql
@@ -0,0 +1,23 @@
+--------------------------------------------------------
+--  Table VARIATION_KPI
+--------------------------------------------------------
+alter table "PCMSDATADEV"."VARIATION_KPI"
+    add EOJ_SECURED NUMBER(19,4) default 0 not null;
+alter table "PCMSDATADEV"."VARIATION_KPI"
+    add EOJ_UNSECURED NUMBER(19,4) default 0 not null;
+alter table "PCMSDATADEV"."VARIATION_KPI"
+    add EOJ_TOTAL NUMBER(19,4) default 0 not null;
+alter table "PCMSDATADEV"."VARIATION_KPI"
+    add EXCEPTION_COMMENT VARCHAR2(255);
+
+--------------------------------------------------------
+--  Table VARIATION_KPI_AUDIT
+--------------------------------------------------------
+alter table "PCMSDATADEV"."VARIATION_KPI_AUDIT"
+    add EOJ_SECURED NUMBER(19,4) default 0 not null;
+alter table "PCMSDATADEV"."VARIATION_KPI_AUDIT"
+    add EOJ_UNSECURED NUMBER(19,4) default 0 not null;
+alter table "PCMSDATADEV"."VARIATION_KPI_AUDIT"
+    add EOJ_TOTAL NUMBER(19,4) default 0 not null;
+alter table "PCMSDATADEV"."VARIATION_KPI_AUDIT"
+    add EXCEPTION_COMMENT VARCHAR2(255);
