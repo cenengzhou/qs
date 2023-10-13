959fefc18ab90cd0f3c06ceffe4a1075ffedfc58
cenengzhou <ceneng.zhou@gammonconstruction.com>
2023-10-13 18:06:46 +0800

    popups style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,67 @@
 import { useState, useRef, useEffect } from 'react'
-import { AppBarComponent, MenuComponent, MenuItemModel, MenuEventArgs } from '@syncfusion/ej2-react-navigations'
-import { DropDownButtonComponent, ItemModel } from '@syncfusion/ej2-react-splitbuttons'
+import { AppBarComponent } from '@syncfusion/ej2-react-navigations'
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons'
 import { DialogComponent } from '@syncfusion/ej2-react-popups'
+import {
+  DropDownButtonComponent,
+  ItemModel,
+} from "@syncfusion/ej2-react-splitbuttons"
 import './style.css'
 import logo from '../../asset/gammon.png'
 
 const Header = () => {
   const [message, setMessage] = useState(false)
   const [profile, setProfile] = useState(false)
+  const [messageP, setMessageP] = useState({X: 0, Y: 0})
+  const [profileP, setprofileP] = useState({X: 0, Y: 0})
+  const messageBtnRef = useRef(null)
+  const profileBtnRef = useRef(null)
   const messageRef = useRef<DialogComponent | null>()
   const profileRef = useRef<DialogComponent | null>()
-  const messageShow = () => {
-    setMessage(true);
+  const messageShow = (e: any) => {
+    setMessage(true)
+    setMessageP({X: e.target.offsetLeft - 280, Y: 56})
   }
-  const profileShow = () => {
-    setProfile(true);
+  const profileShow = (e: any) => {
+    setProfile(true)
+    setprofileP({X: e.target.offsetLeft - 200, Y: 56})
   }
   const messageClose = () => {
-    setMessage(false);
+    setMessage(false)
   }
   const profileClose = () => {
     setProfile(false);
   }
+
   const handleDocumentClick = (e: any) => {
-    if (messageRef.current && !messageRef.current.element.contains(e.target)) {
+    if (e.target !== messageBtnRef.current && messageRef.current && !messageRef.current.element.contains(e.target)) {
       messageClose()
     }
-    if (profileRef.current && !profileRef.current.element.contains(e.target)) {
+    if (e.target !== profileBtnRef.current && profileRef.current && !profileRef.current.element.contains(e.target)) {
       profileClose()
     }
   }
+
+  const roleList: ItemModel[] = [
+    {
+      text: "JOB_ALL",
+    },
+    {
+      text: "ROLE_QS_ENQ",
+    },
+    {
+      text: "ROLE_QS_IMS_ADM",
+    },
+    {
+      text: "ROLE_QS_IMS_ENQ",
+    },
+    {
+      text: "ROLE_QS_QS",
+    },
+    {
+      text: "ROLE_QS_REVIEWER",
+    },
+  ];
 
   useEffect(() => {
     document.addEventListener('click', handleDocumentClick)
@@ -45,24 +76,85 @@ const Header = () => {
         <img src={logo} alt="logo" />
         <span>DEV</span>
       </div>
-      <ButtonComponent cssClass='e-inherit'>Repackaging</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Main Contract Certificate</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Subcontract</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Internal Valuation</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Enquiry</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Reports</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Transit</ButtonComponent>
-      <ButtonComponent cssClass='e-inherit'>Admin</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Repackaging</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">
+        Main Contract Certificate
+      </ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Subcontract</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Internal Valuation</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Enquiry</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Reports</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Transit</ButtonComponent>
+      <ButtonComponent cssClass="e-inherit">Admin</ButtonComponent>
       <div className="header-right">
-        <span className="e-icons e-comments e-large" onClick={() => messageShow()}></span>
+        <span
+          className="e-icons e-comments e-large"
+          onClick={(e) => messageShow(e)}
+          ref={messageBtnRef}
+        ></span>
         <div className="e-avatar">
           <img src={logo} alt="" />
         </div>
-        <div className="username" onClick={() => profileShow()}>cenengzhou<span className="e-icons e-chevron-down-fill e-small"></span></div>
+        <div
+          className="username"
+          onClick={(e) => profileShow(e)}
+          ref={profileBtnRef}
+        >
+          cenengzhou
+          <span className="e-icons e-chevron-down-fill e-small"></span>
+        </div>
         <span className="e-icons e-align-left e-large"></span>
       </div>
-      <DialogComponent id="message" width="300px" visible={message} ref={e => messageRef.current = e}><h1>Message</h1></DialogComponent>
-      <DialogComponent width="300px" visible={profile} ref={e => profileRef.current = e}>Profile</DialogComponent>
+      <DialogComponent
+        position={messageP}
+        id="message"
+        width="315px"
+        visible={message}
+        ref={(e) => (messageRef.current = e)}
+      >
+        <div>
+          <div className="messageHeader">Notifications</div>
+          <div className="messageBody">
+            <span className="e-avatar template-image e-avatar-large e-avatar-circle"></span>
+            <div className="mContent">
+              <div className="mTitle">Year End Cutoff 2022</div>
+              <div className="mTime">
+                Special attention for Singapore users!
+              </div>
+              <span className="e-icons e-bullet-1 e-small"></span>
+            </div>
+          </div>
+          <div className="messageFooter">Go to Announcement Board</div>
+        </div>
+      </DialogComponent>
+      <DialogComponent
+        position={profileP}
+        width="280px"
+        visible={profile}
+        ref={(e) => (profileRef.current = e)}
+      >
+        <div>
+          <div className="profileHeader">
+            <span className="e-avatar template-image e-avatar-circle"></span>
+            <div className="userName">Ce Neng Zhou - System Developer</div>
+            <div className="staff">
+              <small>Staff ID: 000001</small>
+              <DropDownButtonComponent
+                items={roleList}
+                cssClass="e-caret-hide"
+                className="e-success e-small"
+              >
+                Role
+              </DropDownButtonComponent>
+            </div>
+          </div>
+          <div className="profileBody"></div>
+          <div className="profileFooter">
+            <ButtonComponent>Profile</ButtonComponent>
+            <ButtonComponent>Sign out</ButtonComponent>
+          </div>
+        </div>
+      </DialogComponent>
     </AppBarComponent>
   );
 }
